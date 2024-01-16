2ee1a4a00b12f473265edc9c3f6fdb13c22b23e7
Ahmad Amer <ahmadg.aamer2002@gmail.com>
2024-01-16 19:42:36 +0200

    add logic for toggle theme

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+
+const theme = document.getElementsByTagName("body")[0];
+const radioButtons = document.querySelectorAll(".toggle__wrapper input");
+
+radioButtons.forEach((button) => {
+    button.addEventListener('click', event => {
+        if(document.getElementById("dark").checked) {
+            theme.classList = 'dark'; 
+        } else {
+            theme.classList = 'light'; 
+        }
+    });
+});
